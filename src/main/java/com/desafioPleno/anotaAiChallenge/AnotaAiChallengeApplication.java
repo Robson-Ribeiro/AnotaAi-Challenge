cf6d34b8bd14049674a0619b083b3cf7f9c39244
Robson <robsonribeirorra@gmail.com>
2024-02-07 14:55:40 -0300

    first commit

---
+++ src/main/java/com/desafioPleno/anotaAiChallenge/AnotaAiChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.desafioPleno.anotaAiChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotaAiChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotaAiChallengeApplication.class, args);
+	}
+
+}
