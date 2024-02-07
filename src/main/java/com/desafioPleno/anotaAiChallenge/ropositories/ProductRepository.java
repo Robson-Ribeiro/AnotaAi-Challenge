0607c7b65050a85b170b962145b14c2a09111b85
Robson <robsonribeirorra@gmail.com>
2024-02-07 17:25:47 -0300

    Fixing the repositories

--- src/main/java/com/desafioPleno/anotaAiChallenge/ropositories/ProductRepository.java
+++ src/main/java/com/desafioPleno/anotaAiChallenge/ropositories/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.desafioPleno.anotaAiChallenge.ropositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.desafioPleno.anotaAiChallenge.domain.Product.ProductEntity;
 
+@Repository
 public interface ProductRepository extends MongoRepository<ProductEntity, String> {
 }
