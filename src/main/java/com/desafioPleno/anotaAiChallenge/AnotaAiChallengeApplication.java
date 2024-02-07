425f3a303eed67bff60df7d029891db88e3b3f2e
Robson <robsonribeirorra@gmail.com>
2024-02-07 15:10:17 -0300

    Configuring MongoDB

--- src/main/java/com/desafioPleno/anotaAiChallenge/AnotaAiChallengeApplication.java
+++ src/main/java/com/desafioPleno/anotaAiChallenge/AnotaAiChallengeApplication.java
@@ -2,8 +2,10 @@ package com.desafioPleno.anotaAiChallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories()
 public class AnotaAiChallengeApplication {
 
 	public static void main(String[] args) {
