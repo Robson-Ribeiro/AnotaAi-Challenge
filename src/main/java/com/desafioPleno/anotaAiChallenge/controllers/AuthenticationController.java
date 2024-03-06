16cb5161f9e14ef8ade05a43bdba530d253dd7c6
Robson <robsonribeirorra@gmail.com>
2024-03-05 23:54:59 -0300

    Authentication route added

--- src/main/java/com/desafioPleno/anotaAiChallenge/controllers/AuthenticationController.java
+++ src/main/java/com/desafioPleno/anotaAiChallenge/controllers/AuthenticationController.java
@@ -2,20 +2,27 @@ package com.desafioPleno.anotaAiChallenge.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.desafioPleno.anotaAiChallenge.domain.User.UserDto;
 import com.desafioPleno.anotaAiChallenge.security.AuthenticationService;
+
+import jakarta.validation.Valid;
 
 
 @RestController
+@RequestMapping("auth")
+@CrossOrigin
 public class AuthenticationController {
   @Autowired
   private AuthenticationService authenticationService;
 
-  @PostMapping("authenticate")
-  public ResponseEntity<String> authenticate(Authentication authentication) {
-    return ResponseEntity.ok().body(authenticationService.authenticate(authentication));
+  @PostMapping
+  public ResponseEntity<String> authenticate(@RequestBody @Valid UserDto userDto) {
+    return ResponseEntity.ok().body(authenticationService.authenticate(userDto));
   }
 }
--- src/main/java/com/desafioPleno/anotaAiChallenge/security/AuthenticationService.java
+++ src/main/java/com/desafioPleno/anotaAiChallenge/security/AuthenticationService.java
@@ -1,17 +1,26 @@
 package com.desafioPleno.anotaAiChallenge.security;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+
+import com.desafioPleno.anotaAiChallenge.domain.User.UserDto;
 
 @Service
 public class AuthenticationService {
   private JwtService jwtService;
 
-  public AuthenticationService(JwtService jwtService) {
+  private AuthenticationManager authenticationManager;
+
+  public AuthenticationService(JwtService jwtService, AuthenticationManager authenticationManager) {
     this.jwtService = jwtService;
+    this.authenticationManager = authenticationManager;
   }
 
-  public String authenticate(Authentication authentication) {
-    return jwtService.generateToken(authentication);
+  public String authenticate(UserDto data) {
+    var usernamePassword = new UsernamePasswordAuthenticationToken(data.getUsername(), data.getPassword());
+    Authentication auth = this.authenticationManager.authenticate(usernamePassword);
+    return jwtService.generateToken(auth);
   }
 }
